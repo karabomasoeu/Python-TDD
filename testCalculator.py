7aa5c14d0b4af71e98015ef7ea83e65b86dcde14
Karabo <kbmasoeu@gmail.com>
2026-02-06 23:02:13 +0200

    added to test file

--- testCalculator.py
+++ testCalculator.py
@@ -1,2 +1,7 @@
 import unittest
 from calculator import add
+
+class TestCalculator(unittest.TestCase):
+    def test_add_two_numbers(self):
+        result = add(2, 3)
+        self.assertEqual(result, 5)
