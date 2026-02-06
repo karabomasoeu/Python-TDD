a651a6b2fd25d4bcd12e75b2215b96b962cb8130
Karabo <kbmasoeu@gmail.com>
2026-02-06 23:57:29 +0200

    added to test file

--- testCalculator.py
+++ testCalculator.py
@@ -5,3 +5,7 @@ class TestCalculator(unittest.TestCase):
     def test_add_two_numbers(self):
         result = add(2, 3)
         self.assertEqual(result, 5)
+
+    def test_add_negative_numbers(self):
+        result = add(-1, -1)
+        self.assertEqual(result, -2)
