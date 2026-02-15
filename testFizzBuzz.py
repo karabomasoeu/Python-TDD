7d487df43128b05ce5809da709ac0b57593abd71
Karabo <78500285+karabomasoeu@users.noreply.github.com>
2026-02-15 20:35:48 +0200

    Update testFizzBuzz.py

--- testFizzBuzz.py
+++ testFizzBuzz.py
@@ -1,2 +1,7 @@
 import unittest 
 from FizzBuzz import FizzBuzz
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_returns_one_for_one(self):
+        result = fizzbuzz(1)
+        self.assertEqual(result, "1")
