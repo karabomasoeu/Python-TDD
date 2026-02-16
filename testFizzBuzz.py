9ec4dedc6f9dfcd46594de07c0cd5341d33cd9ea
Karabo <78500285+karabomasoeu@users.noreply.github.com>
2026-02-16 23:56:48 +0200

    Update testFizzBuzz.py

--- testFizzBuzz.py
+++ testFizzBuzz.py
@@ -2,9 +2,6 @@ import unittest
 from FizzBuzz import FizzBuzz
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_returns_one_for_one(self):
-        result = fizzbuzz(1)
-        self.assertEqual(result, "1")
-
+    
 if __name__ == '__main__':
     unittest.main()
