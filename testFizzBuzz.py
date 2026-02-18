b8af092de8e4e553127b04b94f03b3b3c4441942
Karabo <78500285+karabomasoeu@users.noreply.github.com>
2026-02-18 22:17:07 +0200

    Update testFizzBuzz.py

--- testFizzBuzz.py
+++ testFizzBuzz.py
@@ -2,6 +2,9 @@ import unittest
 from FizzBuzz import FizzBuzz
 
 class TestFizzBuzz(unittest.TestCase):
+    # Test 1: Check if regular numbers return as string
+    # Test 2: Check if numbers divisible by 3 return "Fizz"
+    # Test 3: Check if numbers divisible by 5 return "Buzz"
     
 if __name__ == '__main__':
     unittest.main()
